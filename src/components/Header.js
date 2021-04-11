92d540794d87de622bcb67714f25bac951427fac
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-04-11 23:48:33 +0800

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,30 +8,34 @@ export default function Header({ }) {
   const onSearch = value => console.log(value);
   return (
     <header className="header">
-      <div className="header-wrap">
-        <div className="header-text" >
-          <Link to='/'>
-            <img className="header-logo" src="./img/logo_tea_da.png"/>
-          </Link>
-          <Link to='/menu' >
-            <p className='header-left-navbar'>菜單</p>
-          </Link>
-          <Link to='/compare' >
-            <p className='header-left-navbar'>飲料比較</p>
-          </Link>
-          <Link to='/comment' >
-            <p className='header-left-navbar'>評論區</p>
-          </Link>
-          <Link to='/' className='header-right-navbar'>
-              <Search placeholder="input search text" className='header-right-search' allowClear onSearch={onSearch} style={{ width: 200 }} />
-          </Link>
-          <Link to='/' className='header-right-navbar'>
-              <img className='header-right-navbar-icon' src="./img/icon_heart.png"/>
-          </Link>
-          <Link to='/' >
-          <img className='header-right-navbar-icon' src="./img/icon_user.png"/>
-          </Link>
-        </div>
+      <div className="header-wrap-left">
+        <Link to='/'>
+          <img className="header-logo" src="./img/logo_tea_da.png"/>
+        </Link>
+      </div>
+      <div className="header-wrap-center">
+        <Link to='/menu' >
+          <p className='header-navbar-center'>菜單</p>
+        </Link>
+        <Link to='/compare' >
+          <p className='header-navbar-center'>飲料比較</p>
+        </Link>
+        <Link to='/comment' >
+          <p className='header-navbar-center'>評論區</p>
+        </Link>
+      </div>
+      <div className="header-wrap-right">
+        <Link to='/' >
+          <div className="header-item">
+            <Search placeholder="想喝什麼？" className='header-search' allowClear onSearch={onSearch} />
+          </div>
+        </Link>
+        <Link to='/' >
+          <img className='header-item header-icon' src="./img/icon_heart.png"/>
+        </Link>
+        <Link to='/' >
+          <img className='header-item header-icon' src="./img/icon_user.png"/>
+        </Link>
       </div>
     </header>
   );
