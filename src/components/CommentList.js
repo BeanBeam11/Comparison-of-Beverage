d9f6a07b99dbf11e9cc4cd40041fb56aca100dea
Lisen <z1x2c3v441@gmail.com>
2021-04-12 14:41:36 +0800

    Comment page select done but the arraw has some position problem

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,40 +1,59 @@
 import { Select } from 'antd';
 import {useState} from "react";
-import kek from "../json/kebuke.json";
+import kebuke from "../json/kebuke.json";
+import fiftylan from "../json/50lan"; 
+import milkshop from "../json/milkshop.json";
+import macu from "../json/macu.json";
+import chingshin from "../json/chingshin.json";
 export default function CommentList() {
+    
     const { Option } = Select;
     const provinceData = ['可不可熟成紅茶', '五十嵐',"麻古茶坊","迷克夏","清心福全"];
-    const cityData = {
-    可不可熟成紅茶: {kek},
-      
-    };
-    const [cities, setCities] = useState(cityData[provinceData[0]]);
-    const [secondCity, setSecondCity] = useState(cityData[provinceData[0]][0]);
+    const [cities, setCities] = useState(kebuke);
+    const [secondCity, setSecondCity] = useState(kebuke.name);
     const handleProvinceChange = value => {
-        setCities(cityData[value]);
-        setSecondCity(cityData[value][0]);
+        switch (value){
+          case ("可不可熟成紅茶"):
+            setCities(kebuke);
+            setSecondCity(kebuke.name);
+          case ("五十嵐"):
+            setCities(fiftylan);
+            setSecondCity(fiftylan.name);
+          case ("麻古茶坊"):
+            setCities(macu);
+            setSecondCity(macu.name);
+          case ("迷克夏"):
+            setCities(milkshop);
+            setSecondCity(milkshop.name);
+          case ("清心福全"):
+            setCities(chingshin);
+            setSecondCity(chingshin.name);
+        }
+
+       
       };
     
       const onSecondCityChange = value => {
         setSecondCity(value);
       };
-    console.log(cityData[provinceData[0]]);
-    console.log(cities)
+    // console.log(cityData);
+    // console.log(cities)
     return(
         <>
-            {/* <div class="comment-add">
+            <div class="comment-add">
                 <img class="comment-add-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpEZZf3-Qc1z5MtboZNbXh-EcVyYQcMyWEbQ&usqp=CAU"/>
                 <Select defaultValue={provinceData[0]} style={{ width: 120 }} onChange={handleProvinceChange}>
         {provinceData.map(province => (
           <Option key={province}>{province}</Option>
         ))}
       </Select>
-      <Select style={{ width: 120 }} value={secondCity} onChange={onSecondCityChange}>
+      <Select defaultValue="請選擇飲料" style={{ width: 120 }} value={secondCity} onChange={onSecondCityChange}>
         {cities.map(city => (
-          <Option key={city}>{city}</Option>
+          
+          <Option key={city.name}>{city.name}</Option>
         ))}
       </Select>
-            </div> */}
+            </div>
         </>
     );
 }
