c39da8c5f36bc195344c3ef8ad69d537a09185a5
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-04-17 13:54:38 +0800

    Decrease Indent and change comment-add-user

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -9,71 +9,68 @@ import {addToComment} from "../actions/";
 import Cookie from "js-cookie";
 import {StoreContext} from "../store"
 export default function AddComment() {
-    const {state:{commentList},dispatch}= useContext(StoreContext);
-    const { Option } = Select;
-    const provinceData = ['可不可熟成紅茶', '五十嵐',"麻古茶坊","迷克夏","清心福全"];
-    const [shops, setShops] = useState(kebuke);
-    const [shopName, setshopName] = useState(kebuke.name);
-    const resource={users:"",shop:"",product:"",description:""};
-    const handleProvinceChange = value => {
-      console.log(value);
-        switch (value){
-          case "可不可熟成紅茶":
-            console.log("kbk");
-            setShops(kebuke);
-            setshopName(kebuke.name);
-            break;
-          case "五十嵐":
-            console.log("50");
-            setShops(fiftylan);
-            setshopName(fiftylan.name);
-            break;
-          case "麻古茶坊":
-            console.log("macu");
-            setShops(macu);
-            setshopName(macu.name);
-            break;
-          case "迷克夏":
-            console.log("milk");
-            setShops(milkshop);
-            setshopName(milkshop.name);
-            break;
-          case "清心福全":
-            console.log("ching");
-            setShops(chingshin);
-            setshopName(chingshin.name);
-            break;
-          
-        }
-      };
-    const Publish=()=> {
-      const resource={users:"Json",shop:"可不可",product:"熟成紅茶",description:"好好喝"};
-      addToComment(dispatch,resource);
-    }
-    useEffect(()=>{
-      Cookie.set("commentList", JSON.stringify(commentList));
-    }, [commentList])
-    return(
-        <>
-            <div class="comment-add">
-                <img class="comment-add-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpEZZf3-Qc1z5MtboZNbXh-EcVyYQcMyWEbQ&usqp=CAU"/>
-                <Select defaultValue={provinceData[0]} style={{ width: 120 }} onChange={handleProvinceChange}>
-        {provinceData.map(province => (
-         
-          <Option key={province}>{province}</Option>
-        ))}
-      </Select>
-      <Select defaultValue="請選擇飲料" style={{ width: 120 }}>
-        {shops.map(city => (
-          <Option key={city.name}>{city.name}</Option>
-          
-        ))}
-      </Select>
+  const {state:{commentList},dispatch}= useContext(StoreContext);
+  const { Option } = Select;
+  const provinceData = ['可不可熟成紅茶', '五十嵐',"麻古茶坊","迷克夏","清心福全"];
+  const [shops, setShops] = useState(kebuke);
+  const [shopName, setshopName] = useState(kebuke.name);
+  const resource={users:"",shop:"",product:"",description:""};
+  const handleProvinceChange = value => {
+    console.log(value);
+      switch (value){
+        case "可不可熟成紅茶":
+          console.log("kbk");
+          setShops(kebuke);
+          setshopName(kebuke.name);
+          break;
+        case "五十嵐":
+          console.log("50");
+          setShops(fiftylan);
+          setshopName(fiftylan.name);
+          break;
+        case "麻古茶坊":
+          console.log("macu");
+          setShops(macu);
+          setshopName(macu.name);
+          break;
+        case "迷克夏":
+          console.log("milk");
+          setShops(milkshop);
+          setshopName(milkshop.name);
+          break;
+        case "清心福全":
+          console.log("ching");
+          setShops(chingshin);
+          setshopName(chingshin.name);
+          break;
+        
+      }
+    };
+  const Publish=()=> {
+    const resource={users:"Json",shop:"可不可",product:"熟成紅茶",description:"好好喝"};
+    addToComment(dispatch,resource);
+  }
+  useEffect(()=>{
+    Cookie.set("commentList", JSON.stringify(commentList));
+  }, [commentList])
+  return(
+    <>
+      <div class="comment-add">
+        <img class="comment-add-img" src="./img/user_note.png"/>
+        <Select defaultValue={provinceData[0]} style={{ width: 120 }} onChange={handleProvinceChange}>
+          {provinceData.map(province => (
+            <Option key={province}>{province}</Option>
+          ))}
+        </Select>
+        <Select defaultValue="請選擇飲料" style={{ width: 120 }}>
+          {shops.map(city => (
+            <Option key={city.name}>{city.name}</Option>
+          ))}
+        </Select>
         <img src="https://image.flaticon.com/icons/png/128/1837/1837512.png" class="comment-add-icon"/>
-      <Input placeholder="寫點評論..." className="comment-input"/>
-      <Button onClick={Publish} className="comment-publish">發布</Button>
-            </div>
-            
-        </>
-    );
+        <Input placeholder="寫點評論..." className="comment-input"/>
+        <Button onClick={Publish} className="comment-publish">發布</Button>
+      </div>  
+    </>
+  );
 }
