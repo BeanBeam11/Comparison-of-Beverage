c867542eaa03c85ff729ecc2091e601aa57f9d17
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-04-19 16:12:38 +0800

    edit footer-center (use Link wrap each icon)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,12 +12,18 @@ export default function Footer() {
                     <img className="footer-icon" src="./img/icon_service.png"/>
                     <p className="footer-text">服務條款</p>
                 </Link>
-                <Link to='/' className="footer-item footer-item-center">
-                    <img className="footer-icon footer-icon-center"src="./img/icon_facebook.png"/>
-                    <img className="footer-icon footer-icon-center" src="./img/icon_instagram.png"/>
-                    <img className="footer-icon footer-icon-center" src="./img/icon_line.png"/>
+                <div className="footer-item footer-item-center">
+                    <Link to='/' >
+                        <img className="footer-icon footer-icon-center"src="./img/icon_facebook.png"/>
+                    </Link>
+                    <Link to='/'>
+                        <img className="footer-icon footer-icon-center" src="./img/icon_instagram.png"/>
+                    </Link>
+                    <Link to='/'>
+                        <img className="footer-icon footer-icon-center" src="./img/icon_line.png"/>
+                    </Link>
                     <p className="footer-text footer-text-center">© 2021 Beverage Comparator</p>
-                </Link>
+                </div>
                 <Link to='/' className="footer-item">
                     <img className="footer-icon" src="./img/icon_question.png"/>
                     <p className="footer-text">常見問題</p>
