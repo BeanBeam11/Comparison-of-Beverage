2fadc38a54193110fecb2fc88bddf44ca54f1578
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-04-19 13:50:51 +0800

    remove div and edit margin-top

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,36 +5,26 @@ export default function Footer() {
         <footer className="footer">
             <div className="footer-content">
                 <Link to='/' className="footer-item">
-                    <div className="footer-item">
-                        <img className="footer-icon" src="./img/icon_privacy.png"/>
-                        <p className="footer-text">隱私政策</p>
-                    </div>
+                    <img className="footer-icon" src="./img/icon_privacy.png"/>
+                    <p className="footer-text">隱私政策</p>
                 </Link>
                 <Link to='/' className="footer-item">
-                    <div className="footer-item">
-                        <img className="footer-icon" src="./img/icon_service.png"/>
-                        <p className="footer-text">服務條款</p>
-                    </div>
+                    <img className="footer-icon" src="./img/icon_service.png"/>
+                    <p className="footer-text">服務條款</p>
                 </Link>
                 <Link to='/' className="footer-item footer-item-center">
-                    <div className="footer-item footer-item-center">
-                            <img className="footer-icon footer-icon-center"src="./img/icon_facebook.png"/>
-                            <img className="footer-icon footer-icon-center" src="./img/icon_instagram.png"/>
-                            <img className="footer-icon footer-icon-center" src="./img/icon_line.png"/>
-                            <p className="footer-text footer-text-center">© 2021 Beverage Comparator</p>
-                    </div>
+                    <img className="footer-icon footer-icon-center"src="./img/icon_facebook.png"/>
+                    <img className="footer-icon footer-icon-center" src="./img/icon_instagram.png"/>
+                    <img className="footer-icon footer-icon-center" src="./img/icon_line.png"/>
+                    <p className="footer-text footer-text-center">© 2021 Beverage Comparator</p>
                 </Link>
                 <Link to='/' className="footer-item">
-                    <div className="footer-item">
-                        <img className="footer-icon" src="./img/icon_question.png"/>
-                        <p className="footer-text">常見問題</p>
-                    </div>
+                    <img className="footer-icon" src="./img/icon_question.png"/>
+                    <p className="footer-text">常見問題</p>
                 </Link>
                 <Link to='/' className="footer-item">
-                    <div className="footer-item">
-                        <img className="footer-icon" src="./img/icon_mail.png"/>
-                        <p className="footer-text">聯絡我們</p>
-                    </div>
+                    <img className="footer-icon" src="./img/icon_mail.png"/>
+                    <p className="footer-text">聯絡我們</p>
                 </Link>
             </div>
         </footer>           
