8c99abf480f807270821c323bef391c95b84f90d
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-06-10 20:08:24 +0800

    edit iphone and ipad RWD, fix home card

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -75,38 +75,44 @@ export default function HomeContent(){
       <div className="home-title-box">
         <p className="home-title">各家主打商品</p>
       </div>
-      <div className="home-card">
-        <div className="home-card-img-box">
-          <img className="home-card-img" src="./img/kebuke_apple_black_tea.jpeg"/>
+      <div className="home-card-wrapper">
+        <div className="home-card">
+          <div className="home-card-img-box">
+            <img className="home-card-img" src="./img/kebuke_apple_black_tea.jpeg"/>
+          </div>
+          <div className="home-card-text">
+            <div className="home-card-title">可不可</div>
+            <div className="home-card-product"> - 春蘋紅茶</div>
+            <div className="home-card-description">酸甜柔和的青森蘋果汁，融合麗春紅茶特有的花意香氣，與粉嫩的櫻花晶球浸潤於口中，逐漸漫入微甘清爽的尾韻，如同遍地群花於心頭紛飛，在大地甦醒的喜悅中，一掃過往的陰霾，為生活揭開新的序幕，帶著輕盈的心，迎向嶄新的一年。
+            </div>
+          </div>
         </div>
-        <div className="home-card-text">
-          <p className="home-card-title">可不可 - 春蘋紅茶</p>
-          <p className="home-card-description">酸甜柔和的青森蘋果汁，融合麗春紅茶特有的花意香氣，與粉嫩的櫻花晶球浸潤於口中，逐漸漫入微甘清爽的尾韻，如同遍地群花於心頭紛飛，在大地甦醒的喜悅中，一掃過往的陰霾，為生活揭開新的序幕，帶著輕盈的心，迎向嶄新的一年。
-          </p>
+        <div className="home-card">
+          <div className="home-card-img-box">
+            <img className="home-card-img" src="./img/macu_pineapple.jpeg"/>
+          </div>
+          <div className="home-card-text">
+            <div className="home-card-title">麻古茶坊</div>
+            <div className="home-card-product"> - 金鑽鳳梨梅蜜</div>
+            <div className="home-card-description">
+              麻古經典「梅蜜🍯」尬上新朋友「金鑽鳳梨🍍」，還有全新配料「葡萄波波🍇」，新鮮金鑽鳳梨與酸甜的梅蜜尬成綿密冰沙✨，搭配上Q彈葡萄波波，清爽解膩❣️回味無窮的新滋味😋
+            </div>
+          </div>
         </div>
+        <div className="home-card">
+          <div className="home-card-img-box">
+            <img className="home-card-img" src="./img/milkshop_strawberry.jpeg"/>
+          </div>
+          <div className="home-card-text">
+            <div className="home-card-title">迷客夏</div>
+            <div className="home-card-product">  - 草莓庫莉鮮奶</div>
+            <div className="home-card-description">
+              選用苗栗大湖香水草莓，在最佳熟度急速冷凍保存製成凍果，完美展現香水草莓的酸甜風味以及細緻香氣，導入法式Coulis輕烹技法，由門市手工費時製成，香甜的天然草莓果漿
+            </div>
+          </div>
+        </div>  
       </div>
-      <div className="home-card">
-        <div className="home-card-img-box">
-          <img className="home-card-img" src="./img/macu_pineapple.jpeg"/>
-        </div>
-        <div className="home-card-text">
-          <p className="home-card-title">麻古茶坊 - 金鑽鳳梨梅蜜</p>
-          <p className="home-card-description">
-            麻古經典「梅蜜🍯」尬上新朋友「金鑽鳳梨🍍」，還有全新配料「葡萄波波🍇」，新鮮金鑽鳳梨與酸甜的梅蜜尬成綿密冰沙✨，搭配上Q彈葡萄波波，清爽解膩❣️回味無窮的新滋味😋
-          </p>
-        </div>
-      </div>
-      <div className="home-card">
-        <div className="home-card-img-box">
-          <img className="home-card-img" src="./img/milkshop_strawberry.jpeg"/>
-        </div>
-        <div className="home-card-text">
-          <p className="home-card-title">迷客夏 - 草莓庫莉鮮奶</p>
-          <p className="home-card-description">
-            選用苗栗大湖香水草莓，在最佳熟度急速冷凍保存製成凍果，完美展現香水草莓的酸甜風味以及細緻香氣，導入法式Coulis輕烹技法，由門市手工費時製成，香甜的天然草莓果漿
-          </p>
-        </div>
-      </div>  
+      
       <BackTop/>
       </>
     );
