3be0313ab08499989bb0b490ee1a43d25a65fef8
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-06-13 16:20:12 +0800

    edit compare, home-card bg

--- src/components/Comparison.js
+++ src/components/Comparison.js
@@ -32,11 +32,13 @@ export default function Comparison(){
                         <div className="compare-items-box">
                             <Button className="compare-delete" onClick={()=>removeComparisonItem(dispatch,item.id)}>x</Button>
                             <img className="compare-img" src={item.image}/>
-                            <p className="compare-items">店家：{item.company}</p>
-                            <p className="compare-items">名稱：{item.name}</p>
-                            <p className="compare-items">價格：{item.price}</p>
-                            <p className="compare-items">評分：{item.grade}</p>
-                            <Button className="compare-items compare-add-to-fav-btn">加入收藏</Button>
+                            <div className="compare-items">名稱：{item.name}</div>
+                            <div className="compare-items">店家：{item.company}</div>
+                            <div className="compare-items">價格：{item.price}</div>
+                            <div className="compare-items">評分：{item.grade}</div>
+                            <div className="compare-items">介紹：...</div>
+                            <div className="compare-items">熱量：...</div>
+                            <Button className="compare-items compare-fav-btn">加入收藏</Button>
                         </div>
                    
                 ))}
